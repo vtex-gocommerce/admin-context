0430794332cd00f9ed8794d53da3e59fecb24536
Gustavo Rosolem <guspace92@hotmail.com>
2019-10-03 12:51:02 -0300

    Get user data from admin-profile-graphql

--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -1,7 +1,3 @@
-import cookie from 'cookie'
-// @ts-ignore -> this lib not even exists :eyes:
-import * as JWT from 'jwt-js'
-import Md5 from 'js-md5'
 import { loggerMiddleware, buildGraphQLError } from '@gocommerce/utils'
 
 const splunkToken = 'dd433cc0-9106-4b0d-883a-377d57e8eb1a'
@@ -36,20 +32,6 @@ export const resolvers = loggerMiddleware(splunkToken, {
 
       return data
     },
-    user: async function(_: any, _param: Params, ctx: Context) {
-      const cookies = cookie.parse(ctx.request.header.cookie)
-      const decodedJwt = JWT.decodeToken(cookies._ssid)
-      const email = decodedJwt.payload.sub
-      const avatar = `https://www.gravatar.com/avatar/${Md5(email)}.png?d=mp`
-      const name = ''
-      const createdAt = ''
-      return {
-        avatar,
-        email,
-        name,
-        createdAt
-      }
-    }
   },
   Mutation: {
     blankMutation: async function(_: any, _param: Params, _ctx: Context) {
--- react/SetUserData.tsx
+++ react/SetUserData.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import { graphql, compose } from 'react-apollo'
-import getUser from './queries/user.gql'
+import getProfile from './queries/getProfile.gql'
 
 interface HandleAccountProps {
   UserContext
   getUser: {
-    user
+    getUserData: {
+      name: string
+      email: string
+      picture: string
+    }
     loading: boolean
     refetch: Function
   }
@@ -13,15 +17,20 @@ interface HandleAccountProps {
 
 class SetUserData extends React.Component<HandleAccountProps, any> {
   render() {
-    const userData = this.props.getUser && this.props.getUser.user ? this.props.getUser.user : null
-    const refetch = this.props.getUser && this.props.getUser.refetch ? this.props.getUser.refetch : () => {}
-    const { UserContext, children } = this.props
-    return <UserContext.Provider value={{ userData, refetch, query: getUser }}>{children}</UserContext.Provider>
+    const { getUser, UserContext, children } = this.props
+    const userDataReturn = getUser && getUser.getUserData ? getUser.getUserData : null
+    const userData = userDataReturn && {
+      avatar: userDataReturn.picture,
+      email: userDataReturn.email,
+      name: userDataReturn.name,
+    } || {}
+    const refetch = getUser && getUser.refetch ? getUser.refetch : () => {}
+    return <UserContext.Provider value={{ userData, refetch, query: getProfile }}>{children}</UserContext.Provider>
   }
 }
 
 export default compose(
-  graphql(getUser, {
+  graphql(getProfile, {
     name: 'getUser',
     options: props => ({
       ssr: false,
